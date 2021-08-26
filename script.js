2c36db0e7b490e9f19794ec0e58c502547b0082c
DakotaStorm <85202807+DakotaStorm@users.noreply.github.com>
2021-08-26 11:04:05 -0600

    Scroll Trigger

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+gsap.registerPlugin(ScrollTrigger);
 const timeline = gsap.timeline({ defaults: { duration: 1.5 } });
 
 timeline
   .from(".header", { x: "-100%", ease: "power1" })
   .from(".vision", { x: "-120%", ease: "slow" })
-  .from(".vision-text", { duration: 4, opacity: 0 })
-  .from(".who-we-need", { x: "120%", ease: "slow" }, 6)
-  .from(".who-text", { duration: 4, opacity: 0 }, 8)
-  .from(".footer", { x: "120%", ease: "power1" }, 8);
+  .from(".vision-text", { duration: 4, opacity: 0 });
+
+const tl = gsap.timeline({
+  scrollTrigger: { trigger: ".who-we-need", start: "top center" },
+});
+tl.from(".who-we-need", { duration: 1.5, x: "120%", ease: "slow" })
+  .from(".who-text", { duration: 4, opacity: 0 })
+  .from(".footer", { duration: 1.5, x: "120%", ease: "power1" });
 
